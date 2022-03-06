d88b05f607b6afa76a5db97a393157117fb599ee
Bruno <bdelafontaine92@gmail.com>
2022-03-06 13:23:54 -0500

    Make use of mongoDB for posts

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+const mongoose = require('mongoose');
+
+
+const mongo_key = process.env.MONGOKEY
+mongoose.connect("mongodb+srv://admin-bruno:" + mongo_key + "@cluster0.o3vpt.mongodb.net/blogDB")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-var posts=[]
+const postSchema = new mongoose.Schema ({
+  title: String,
+  text: String
+})
+
+const Post = mongoose.model("Post", postSchema)
+
+
 
 const app = express();
 
@@ -34,35 +46,41 @@ app.get("/compose", (req,res) => {
 
 app.post("/compose", (req,res) => {
 
-  const post = {
+
+  const post = new Post ( {
     title : req.body.titleBox,
     text : req.body.textBox,
-  }
-  posts.push(post)
+  })
+  post.save()
+
   res.redirect("/")
 })
 
 app.get("/", (req,res) => {
-
-  res.render("home", {homeContent:homeStartingContent,
-                      posts:posts})
-})
-
-app.get("/post/:day", (req,res) => {
-
-  const requested_title = req.params.day
-  var resquested_post =""
-  posts.forEach(function(post) {
-    if(_.lowerCase(post.title) === _.lowerCase(requested_title)) {
-      resquested_post = post
+  Post.find({}, function(err, posts){
+    if(!err) {
+      res.render("home", {homeContent:homeStartingContent,
+        posts:posts})
+    } else {
+      res.render("home", {homeContent:homeStartingContent,
+        posts:[]})
     }
   })
 
-  if (resquested_post === "") {
-    res.redirect("/")
-  } else {
-    res.render("post", {title:resquested_post.title,text:resquested_post.text})
-  }
+})
+
+app.get("/post/:id", (req,res) => {
+
+  const requested_id = req.params.id
+
+  // search the id
+  Post.findOne({_id: requested_id}, function(err, foundPost) {
+    if(!err) {
+      res.render("post", {title:foundPost.title,text:foundPost.text})
+    } else{
+      res.redirect("/")
+    }
+  })
 
 })
 
